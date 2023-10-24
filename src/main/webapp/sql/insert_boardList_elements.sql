00af3bbf1f681b3396f7090bf25ea6668b51d43a
naka0303 <jyunki.nakagawa@gmail.com>
2023-10-25 01:36:32 +0900

    コマ配置を画像クリックでできるよう修正

---
+++ src/main/webapp/sql/insert_boardList_elements.sql
@@ -0,0 +1,5 @@
+INSERT INTO boardlist_elements VALUES (1, '黒コマ', '/img/black_disc.jpeg');
+
+INSERT INTO boardList_elements VALUES (2, '白コマ', '/img/white_disc.jpeg');
+
+INSERT INTO boardList_elements VALUES (3, '盤面マス', '/img/board.jpeg');
